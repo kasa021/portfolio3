d235e84ee7cededa7b7a52753cff047196ec0824
kasa021 <mkafa.k11@gmail.com>
2023-05-28 15:23:42 +0900

    feat: 初めの画面を作成

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import { BsGithub, BsTwitter } from "react-icons/bs";
+
 export const Home = () => {
+  const handleGithubClick = () => {
+    window.open("https://github.com/kasa021", "_blank");
+  };
+
+  const handleTwitterClick = () => {
+    window.open("https://twitter.com/kAsA02_", "_blank");
+  };
+
   return (
     <div>
-      <h1>Index</h1>
+      <img
+        src="../public/me.jpg"
+        alt="me"
+        style={{ width: "100px", height: "100px" }}
+      />
+      <h1>Kasa</h1>
+      <div>
+        <button onClick={handleGithubClick}>
+          <BsGithub />
+        </button>
+        <button onClick={handleTwitterClick}>
+          <BsTwitter />
+        </button>
+      </div>
     </div>
   );
 };
