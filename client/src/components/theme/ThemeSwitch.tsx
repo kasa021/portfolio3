20fc2b3f9d3969cd89a7fdc513e1defc3501e4bf
kasa021 <mkafa.k11@gmail.com>
2023-05-28 20:45:36 +0900

    style: ダークモードの色を適用

--- client/src/components/theme/ThemeSwitch.tsx
+++ client/src/components/theme/ThemeSwitch.tsx
@@ -18,7 +18,7 @@ export const ThemeSwitch: React.FC = () => {
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className={styles.button}
     >
-      {theme === "dark" ? <TbSunFilled /> : <TbMoonFilled />}
+      {theme === "dark" ? <TbSunFilled  className={styles.icon} /> : <TbMoonFilled className={styles.icon} />}
     </button>
   );
 };
