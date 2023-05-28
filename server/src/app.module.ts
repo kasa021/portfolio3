151eb95ea97fd518fd885cee8452627dfb4a37d5
kasa021 <mkafa.k11@gmail.com>
2023-05-29 00:44:29 +0900

    feat: messageをdiscordに送信する機能を作成

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MessageController } from './message/message.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MessageController],
+  providers: [],
 })
 export class AppModule {}
