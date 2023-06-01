06ae2226d2c81ae0dd0c7b4ec3d08c8e8bf73611
kasa021 <mkafa.k11@gmail.com>
2023-06-01 13:30:22 +0900

    feat:nestjsで.mdファイルを取得する昨日を追加

---
+++ client/src/components/blog/BlogList.tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface Blog {
+  filename: string;
+}
+
+export const BlogList = () => {
+  const [blogList, setBlogList] = useState<Blog[]>([]);
+   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchBlogList = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/blog");
+        const data = await response.json();
+        setBlogList(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching blog list:", error);
+      }
+    };
+
+    fetchBlogList();
+  }, []);
+
+  const handleClick = async (filename: string) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/blog/${filename}`
+      );
+      navigate(`/blog/${filename}`, {
+        state: { markdownContent: response.data },
+      });
+    } catch (error) {
+      console.error("Error fetching markdown file:", error);
+    }
+  };
+
+  return (
+    <div>
+      {/* ブログリストをマップしてボタンを表示 */}
+      {blogList.map((blog) => (
+        <button key={blog.filename} onClick={() => handleClick(blog.filename)}>
+          {blog.title}
+        </button>
+      ))}
+    </div>
+  );
+};
