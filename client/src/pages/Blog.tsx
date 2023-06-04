676e38127a8ff41ab9f37363460dc0967c7c089c
kasa021 <mkafa.k11@gmail.com>
2023-06-04 11:59:43 +0900

    style: blogページのスタイルをいい感じに設定

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -2,10 +2,6 @@ import { BlogList } from "../components/blog/BlogList";
 
 export const Blog = () => {
   return (
-    <div>
-      <p>akjldjfakljfd;akljf</p>
-      <p>klasjflaksdjfklsajdfl;ksadjf</p>
       <BlogList />
-    </div>
   );
 };
