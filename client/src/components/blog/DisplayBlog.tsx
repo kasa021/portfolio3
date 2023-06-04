6c254d184b0a50f8838e24a01338891ae4692d55
kasa021 <mkafa.k11@gmail.com>
2023-06-04 15:08:00 +0900

    feat: apiのエンドポイントを修正

--- client/src/components/blog/DisplayBlog.tsx
+++ client/src/components/blog/DisplayBlog.tsx
@@ -1,11 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 import styles from "./DisplayBlog.module.css";
 
 export const DisplayBlog = () => {
   const location = useLocation();
-  const markdownContent = location?.state?.markdownContent || "";
+  const { filename } = useParams();
+  const [markdownContent, setMarkdownContent] = useState(
+    location?.state?.markdownContent || ""
+  );
+
+  useEffect(() => {
+    const fetchMarkdownContent = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/api/blog/${filename}`
+        );
+        setMarkdownContent(response.data);
+      } catch (error) {
+        console.error("Error fetching markdown file:", error);
+      }
+    };
+
+    if (!markdownContent) {
+      fetchMarkdownContent();
+    }
+  }, [filename, markdownContent]);
 
   return (
     <div className={styles.container}>
