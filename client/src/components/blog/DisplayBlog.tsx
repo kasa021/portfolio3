746212a61152f06d9ab68c281b04b7839d305390
kasa021 <mkafa.k11@gmail.com>
2023-06-04 20:21:03 +0900

    style: ブログ記事のコードのスタイルを設定

--- client/src/components/blog/DisplayBlog.tsx
+++ client/src/components/blog/DisplayBlog.tsx
@@ -2,6 +2,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { monokai } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import remarkGfm from "remark-gfm";
 
 import styles from "./DisplayBlog.module.css";
 
@@ -25,7 +28,33 @@ export const DisplayBlog = () => {
 
   return (
     <div className={styles.container}>
-      <ReactMarkdown>{markdownContent}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, style, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={monokai}
+                language={match[1]}
+                PreTag="div"
+                codeTagProps={{
+                  style: { fontFamily: "Consolas,monaco,monospace" },
+                }}
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdownContent}
+      </ReactMarkdown>
     </div>
   );
 };
