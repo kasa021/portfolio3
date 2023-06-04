61825aa37c89980ec8d1143c27a8c0f81fcfaa4c
kasa021 <mkafa.k11@gmail.com>
2023-06-04 23:16:49 +0900

    fix: themeの型エラーを解消

--- client/src/components/theme/ThemeProvider.tsx
+++ client/src/components/theme/ThemeProvider.tsx
@@ -6,13 +6,15 @@ type ThemeProviderProps = {
   children: React.ReactNode;
 };
 
+type Theme = "light" | "dark";
+
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const initialTheme = localStorage.getItem("theme") || "light";
+  const initialTheme = localStorage.getItem("theme") as Theme || "light";
   const [theme, setTheme] = useState(initialTheme);
 
   useEffect(() => {
     document.body.dataset.theme = theme;
-  }, [theme]);
+  }, [theme]);  
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
