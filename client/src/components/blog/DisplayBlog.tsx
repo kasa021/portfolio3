faa2df997016613b6541684033760041eb46cfc9
kasa021 <mkafa.k11@gmail.com>
2023-06-04 11:59:43 +0900

    style: blogページのスタイルをいい感じに設定

--- client/src/components/blog/DisplayBlog.tsx
+++ client/src/components/blog/DisplayBlog.tsx
@@ -1,12 +1,14 @@
 import ReactMarkdown from "react-markdown";
 import { useLocation } from "react-router-dom";
 
+import styles from "./DisplayBlog.module.css";
+
 export const DisplayBlog = () => {
   const location = useLocation();
   const markdownContent = location?.state?.markdownContent || "";
 
   return (
-    <div>
+    <div className={styles.container}>
       <ReactMarkdown>{markdownContent}</ReactMarkdown>
     </div>
   );
