6e016a94168435c8425138633d3f62575bbe9c3e
kasa021 <mkafa.k11@gmail.com>
2023-06-04 15:07:22 +0900

    chore: コメントを追加

--- client/src/components/blog/BlogList.tsx
+++ client/src/components/blog/BlogList.tsx
@@ -14,28 +14,28 @@ interface RootState {
 
 
 export const BlogList = () => {
-  const blogList = useSelector((state: RootState) => state.blog.list);
-  const dispatch = useDispatch();
+  const blogList = useSelector((state: RootState) => state.blog.list);  // useSelectorを使ってReduxのstoreからブログ一覧を取得
+  const dispatch = useDispatch();  // useDispatchを使ってReduxのstoreにアクセス
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchBlogList = async () => {
-      if (blogList.length === 0) {
+    const fetchBlogList = async () => {  // ブログ一覧を取得する関数を定義
+      if (blogList.length === 0) {  // ブログ一覧が空の場合のみ取得する
         try {
-          const response = await axios.get("http://localhost:3001/blog");
-          dispatch(setBlogList(response.data));
+          const response = await axios.get("http://localhost:3001/api/blog");
+          dispatch(setBlogList(response.data));  // ブログ一覧をReduxのstoreに保存
         } catch (error) {
           console.error("Error fetching blog list:", error);
         }
       }
     };
     fetchBlogList();
-  }, [blogList, dispatch]);
+  }, [blogList, dispatch]);  // blogListとdispatchをuseEffectの第二引数に指定
 
   const handleClick = async (filename: string) => {
     try {
       const response = await axios.get(
-        `http://localhost:3001/blog/${filename}`
+        `http://localhost:3001/api/blog/${filename}`
       );
       navigate(`/blog/${filename}`, {
         state: { markdownContent: response.data },
@@ -47,14 +47,15 @@ export const BlogList = () => {
 
   return (
     <div className={styels.container}>
-      <div>
+      <h1>ブログ一覧</h1>
+      <div className={styels.buttonContainer}>
         {blogList.map((blog) => (
           <button
             key={blog}
             onClick={() => handleClick(blog)}
             className={styels.button}
           >
-            {blog}
+            {blog.slice(0, -3)}
           </button>
         ))}
       </div>
