0102708933c6a368fd7afb69f211102872dc03c7
kasa021 <mkafa.k11@gmail.com>
2023-06-07 07:05:14 +0900

    style: hタグの深さによって、表示位置をずらす

--- client/src/components/blog/sidebar/Toc.tsx
+++ client/src/components/blog/sidebar/Toc.tsx
@@ -1,21 +1,49 @@
 import styles from "./Toc.module.css";
 
 interface TocProps {
-  toc: string[];
+  toc: { text: string; depth: number }[];
 }
 
 export const Toc = (props: TocProps) => {
+  const renderTocItems = (headings: { text: string; depth: number }[]) => {
+    let prevDepth = 0;
+    return headings.map((heading, index) => {
+      const { text, depth } = heading;
+      const indentClass = styles[`indent${depth}`];
+
+      if (depth > prevDepth) {
+        prevDepth = depth;
+        return (
+          <div key={index} className={`${styles.tocItem} ${indentClass}`}>
+            <a href={`#${text}`}>{text}</a>
+          </div>
+        );
+      } else if (depth < prevDepth) {
+        prevDepth = depth;
+        return (
+          <div
+            key={index}
+            className={`${styles.tocItem} ${styles.decreaseIndent} ${indentClass}`}
+          >
+            <a href={`#${text}`}>{text}</a>
+          </div>
+        );
+      } else {
+        return (
+          <div key={index} className={`${styles.tocItem} ${indentClass}`}>
+            <a href={`#${text}`}>{text}</a>
+          </div>
+        );
+      }
+    });
+  };
 
   return (
     <section className={styles.toc}>
       <p className={styles.tocTitle}>目次</p>
       <div className={styles.tocList}>
         {props.toc.length === 0 && <p>見出しがありません</p>}
-        {props.toc.map((heading, index) => (
-          <div key={index} className={styles.tocItem}>
-            <a href={`#${heading}`}>{heading}</a>
-          </div>
-        ))}
+        {renderTocItems(props.toc)}
       </div>
     </section>
   );
--- client/src/components/blog/sidebar/getHeading.ts
+++ client/src/components/blog/sidebar/getHeading.ts
@@ -1,11 +1,13 @@
 export const getHeadings = () => {
   const headings = Array.from(document.querySelectorAll("h1, h2, h3, h4")).map(
-    (heading) =>{
+    (heading) => {
+      const depth = parseInt(heading.tagName.substring(1)); // 見出しの深さを取得
       heading.id = heading.textContent || "";
-      return heading.textContent;
+      return { text: heading.textContent, depth };
     }
   );
-  const nullPoppedHeadings = headings.filter((heading) : heading is string => heading !== null);
+  const nullPoppedHeadings = headings.filter(
+    (heading) => heading.text !== null
+  );
   return nullPoppedHeadings;
 };
-
