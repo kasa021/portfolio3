cc1d4723ec868975b1779f64e79278e9bd529e39
kasa021 <mkafa.k11@gmail.com>
2023-06-07 07:13:44 +0900

    fix: tocの型エラーを解消

--- client/src/components/blog/sidebar/getHeading.ts
+++ client/src/components/blog/sidebar/getHeading.ts
@@ -1,9 +1,15 @@
-export const getHeadings = () => {
+interface TocProps {
+  text: string;
+  depth: number;
+}
+
+export const getHeadings = (): TocProps[] => {
   const headings = Array.from(document.querySelectorAll("h1, h2, h3, h4")).map(
     (heading) => {
       const depth = parseInt(heading.tagName.substring(1)); // 見出しの深さを取得
       heading.id = heading.textContent || "";
-      return { text: heading.textContent, depth };
+      const text = heading.textContent ?? ""; // nullの場合は空文字列を指定
+      return { text, depth };
     }
   );
   const nullPoppedHeadings = headings.filter(
