32861a624898f1f4dee6a1d467343d08d5c466d9
kasa021 <mkafa.k11@gmail.com>
2023-06-13 02:56:41 +0900

    fix: 環境変数の名前の頭文字にVITEを追加

--- client/src/components/blog/BlogList.tsx
+++ client/src/components/blog/BlogList.tsx
@@ -18,7 +18,9 @@ export const BlogList = () => {
       if (blogList.length === 0) {
         // ブログ一覧が空の場合のみ取得する
         try {
-          const response = await axios.get(`${import.meta.env.SERVER_BASE_URL}/api/blog`);
+          const response = await axios.get(
+            `${import.meta.env.VITE_SERVER_BASE_URL}/api/blog`
+          );
           setBlogList(response.data);
         } catch (error) {
           console.error("Error fetching blog list:", error);
